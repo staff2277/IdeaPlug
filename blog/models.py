736ad98539b528e8493cd5a4abccabae145d98c0
Mustapha Osman <wnfynn@gmail.com>
2025-04-25 16:33:24 +0000

    building auth

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class MyBlog(models.Model):
+    title = models.CharField(max_length=200)
+    sub_title = models.CharField(max_length=250)
+    
