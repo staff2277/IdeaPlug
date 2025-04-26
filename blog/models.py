c7b4f52c72bf168ffd8dade44ecb89dcd21372de
Mustapha Osman <wnfynn@gmail.com>
2025-04-26 16:43:50 +0000

    Implement django-browser-reload functionality with middleware, views, and templates
    - Added django-browser-reload app configuration and necessary URLs.
    - Implemented middleware to inject reload script into HTML responses.
    - Created views to handle event streaming for browser reloads.
    - Developed JavaScript files for handling reload events and worker communication.
    - Integrated Jinja template support for dynamic reloading.
    - Updated base HTML template to include the reload script.
    - Added user forms for authentication features.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class MyBlog(models.Model):
+
+
+class Blog(models.Model):
     title = models.CharField(max_length=200)
     sub_title = models.CharField(max_length=250)
+    bg_image = models.ImageField(upload_to= 'media/', blank=True, null=True)
+    main_content = models.TextField()
+
+    
     
--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
     path('', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
