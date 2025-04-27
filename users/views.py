51bbe07da153d5d5a4d0e1c6a1efb8c53e1b780b
Mustapha Osman <wnfynn@gmail.com>
2025-04-27 13:42:03 +0000

    Update SignupView to set success URL using reverse_lazy and implement form validation

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views.generic import FormView
 from .forms import UserRegistration
 
@@ -5,4 +6,9 @@ from .forms import UserRegistration
 class SignupView(FormView):
     template_name = "users/signup.html"
     form_class = UserRegistration
-    success_url = "home"
+    success_url = reverse_lazy("blog:home")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
