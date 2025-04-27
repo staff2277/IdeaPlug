d3f4bfe1c5b8a8a41babf4c0d2cd98868cf9ef67
Mustapha Osman <wnfynn@gmail.com>
2025-04-27 15:21:49 +0000

    Refactor blog models and views; rename Blog class to BlogPost and update NewBlogBView for improved clarity and structure

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 
+User = get_user_model()
 
-class Blog(models.Model):
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     sub_title = models.CharField(max_length=250)
-    bg_image = models.ImageField(upload_to= 'media/', blank=True, null=True)
+    bg_image = models.ImageField(upload_to="media/", blank=True, null=True)
     main_content = models.TextField()
-
-    
-    
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
+from .models import BlogPost
 
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'blog/home.html'
+    template_name = "blog/home.html"
 
 
-
-
-
+class NewBlogBView(CreateView):
+    model = BlogPost
+    template_name = "blog/new_blog.html"
+    fields = [
+        "title",
+        "sub_title",
+        "bg_image",
+    ]
+    success_url = reverse_lazy("blog:home")
