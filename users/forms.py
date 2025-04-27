3d43a304e804f3ea868fd5a26b39131d029bf858
Mustapha Osman <wnfynn@gmail.com>
2025-04-27 13:00:51 +0000

    Refactor user registration form to include email field and update signup view

--- users/forms.py
+++ users/forms.py
@@ -4,17 +4,17 @@ from django.contrib.auth.models import User
 
 
 class UserRegistration(UserCreationForm):
-    email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=50, required=True)
     last_name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(max_length=50, required=True)
 
     class Meta:
         model = User
         fields = [
             "username",
-            "email",
             "first_name",
             "last_name",
+            "email",
             "password1",
             "password2",
         ]
--- users/views.py
+++ users/views.py
@@ -1,16 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-from django.urls import reverse_lazy
 from django.views.generic import FormView
-
-# Create your views here.
+from .forms import UserRegistration
 
 
 class SignupView(FormView):
-    template_name = "users/signup.html"
-    form_class = UserCreationForm
-    success_url = reverse_lazy("home")
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+    template_name = 'users/signup.html'
+    form_class = UserRegistration
