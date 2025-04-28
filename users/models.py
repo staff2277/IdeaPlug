d22f03a76b89aab04db87f2ab18ac574a14bdfba
Mustapha Osman <wnfynn@gmail.com>
2025-04-28 15:14:20 +0000

    Update binary files for model cache and database

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='media/',blank=True, null=True)
+    avatar = models.ImageField(upload_to="media/", blank=True, null=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f"{self.user.username} Profile"
