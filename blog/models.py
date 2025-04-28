5c2f3e1438fcb4e031746ec932bbe3b108762cd2
Mustapha Osman <wnfynn@gmail.com>
2025-04-28 15:06:02 +0000

    Add Employee model to manage employee details

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,16 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return f"user => {self.user} || title => {self.title}"
+
+
+class Employee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=7)
+    marriage_status = models.CharField(max_length=10)
+    religion = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name} || {self.position}"
